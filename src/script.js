23ce588efcaee1babbb2a8083e14156334e055a4
Ageu Silva <39139593+Ageursilva@users.noreply.github.com>
2026-01-14 21:22:11 -0300

    (Feat)Criação de função de calcular idade

--- src/script.js
+++ src/script.js
@@ -1,34 +1,46 @@
 $(document).ready(function () {
+
     var $dataInput = $('#data_nascimento');
-    if ($dataInput.length === 0) return;
+    if ($dataInput.length) {
+        var hoje = new Date();
+        var hojeISO = hoje.toISOString().split('T')[0];
+        $dataInput.attr('max', hojeISO);
 
+        var $helper = $dataInput.closest('.input-field').find('.helper-text');
 
-    var hoje = new Date();
-    var hojeISO = hoje.toISOString().split('T')[0];
-    $dataInput.attr('max', hojeISO);
+        $dataInput.on('change', function () {
+            var value = $(this).val();
 
-    var $helper = $dataInput.closest('.input-field').find('.helper-text');
+            if (!value) {
+                $(this).removeClass('invalid valid');
+                if ($helper.length) $helper.text('');
+                return;
+            }
 
-    $dataInput.on('change', function () {
-        var value = $(this).val();
+            var selected = new Date(value);
+            var hojeDate = new Date();
+            hojeDate.setHours(0, 0, 0, 0);
+            selected.setHours(0, 0, 0, 0);
 
-        if (!value) {
-            $(this).removeClass('invalid valid');
-            if ($helper.length) $helper.text('');
-            return;
+            if (selected > hojeDate) {
+                $(this).addClass('invalid').removeClass('valid');
+                if ($helper.length) $helper.text('Data de nascimento não pode ser futura');
+            } else {
+                $(this).removeClass('invalid').addClass('valid');
+                if ($helper.length) $helper.text('');
+            }
+        });
+    }
+    function calularIdade(dataNascISO) {
+        var hoje = new Date();
+        var nasc = new Date(dataNascISO);
+        var idade = hoje.getFullYear() - nasc.getFullYear();
+        var mes = hoje.getMonth() - nasc.getMonth();
+        if (mes < 0 || (mes === 0 && hoje.getDate() < nasc.getDate())) {
+            idade--;
         }
-
-        var selected = new Date(value);
-        var hojeDate = new Date();
-        hojeDate.setHours(0, 0, 0, 0);
-        selected.setHours(0, 0, 0, 0);
-
-        if (selected > hojeDate) {
-            $(this).addClass('invalid').removeClass('valid');
-            if ($helper.length) $helper.text('Data de nascimento não pode ser futura');
-        } else {
-            $(this).removeClass('invalid').addClass('valid');
-            if ($helper.length) $helper.text('');
-        }
-    });
+    }
+    return idade;
 });
+
+
